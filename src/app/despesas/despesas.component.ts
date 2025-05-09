cd4b5eee8c4f556a831c5e8be714e8c766dde842
emanuel120021 <emanuel.lazaro.52@gmail.com>
2025-05-08 23:25:07 -0300

    adiciona a propriedade standalone aos componentes Despesas e Metas

--- src/app/despesas/despesas.component.ts
+++ src/app/despesas/despesas.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-despesas',
   imports: [],
+  standalone: true,
   templateUrl: './despesas.component.html',
-  styleUrl: './despesas.component.scss'
+  styleUrl: './despesas.component.scss',
 })
-export class DespesasComponent {
-
-}
+export class DespesasComponent {}
--- src/app/metas/metas.component.ts
+++ src/app/metas/metas.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-metas',
   imports: [],
+  standalone: true,
   templateUrl: './metas.component.html',
-  styleUrl: './metas.component.scss'
+  styleUrl: './metas.component.scss',
 })
-export class MetasComponent {
-
-}
+export class MetasComponent {}
